500ef5e21f2310909f30d1b20538031d7f88f449
Abhinav Kumar <kumarabhinav75@gmail.com>
2019-07-22 02:33:48 +0530

    removed store logic from index

--- src/index.js
+++ src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './redux/reducers/index';
-
-const store = createStore(
-    rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
 
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root'));
+ReactDOM.render(<App />,document.getElementById('root'));
