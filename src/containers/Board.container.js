58bfea9a7a889f06e00eeddb905003fe9371b6e8
Abhinav Kumar <kumarabhinav75@gmail.com>
2019-07-22 01:06:19 +0530

    created a board container

---
+++ src/containers/Board.container.js
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux'
+import { addList, addCard } from '../redux/actions'
+import Board from '../components/Board/Board.component'
+
+const mapStateToProps = state => ({
+    cardData: state.cards,
+    listNames: state.list
+})
+
+const mapDispatchToProps = dispatch => ({
+    addList: listName => dispatch(addList(listName)),
+    addCard: payload => dispatch(addCard(payload))
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Board)
